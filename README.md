e61c7de6c8cac497e26222e581b5e3f358b00885
HighonAces <srjnreddy33@gmail.com>
2021-10-30 13:20:19 -0500

    Added extra files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This is a new repository for betting website
+This is a new repository for betting website. This is a personal project 
 
---
+++ src/main/java/com/srujanreddy/bettingsite/BettingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.srujanreddy.bettingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettingSiteApplication.class, args);
+    }
+
+}
